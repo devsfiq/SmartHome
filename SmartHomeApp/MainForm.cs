bf0f350bbaa218962062986e6040487a01f24d6d
Siddiq <devsfiq@gmail.com>
2018-01-25 21:56:24 +0800

    Initial Commit

---
+++ SmartHomeApp/MainForm.cs
@@ -0,0 +1,45 @@
+﻿using SmartHomeLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmartHomeApp
+{
+    public partial class MainForm : Form
+    {
+
+        public SmartHomeServer Server;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            String ip = "127.0.0.1";
+            int port = 3000;
+
+            Server = new SmartHomeServer(ip, port);
+            Server.Start(ProcessCommand);
+        }
+
+        private void ProcessCommand(string command)
+        {
+            MessageBox.Show($"Command Received: {command}", "Smart Home Server");
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Server.Stop();
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Server.Stop();
+        }
+    }
+}
---
+++ SmartHomeLib/SmartHomeServer.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SmartHomeLib
+{
+    public class SmartHomeServer
+    {
+        public delegate void OnReceiveCommand(string command); // Callback Method
+
+        public readonly string Ip;
+        public readonly int Port;
+
+        private Thread listenerThread;
+        private TcpListener Listener;
+        private OnReceiveCommand onReceiveCommand;
+        private int count;
+
+        public SmartHomeServer(string ip, int port)
+        {
+            Ip = ip;
+            Port = port;
+            Listener = new TcpListener(IPAddress.Parse(ip), port);
+        }
+
+        public void Start(OnReceiveCommand onReceiveCommand)
+        {
+            this.onReceiveCommand = onReceiveCommand;
+            count = 0;
+
+            listenerThread = new Thread(RunListenerThread);
+            listenerThread.Start();
+        }
+
+
+        public void Stop()
+        {
+            Listener.Stop();
+            listenerThread.Abort();
+        }
+
+        private void RunListenerThread()
+        {
+            Listener.Start();
+            Socket client = Listener.AcceptSocket();
+            var childSocketThread = new Thread(() =>
+            {
+                ++count;
+                String request = "";
+
+                byte[] data = new byte[256];
+                int size = client.Receive(data);
+
+                for (int i = 0; i < size; i++)
+                {
+                    Char c = Convert.ToChar(data[i]);
+                    request += c;
+                    if (c == '\n') break;
+                }
+
+                string startStr = "/";
+                string endStr = "HTTP";
+
+                int start = request.IndexOf(startStr) + startStr.Length;
+                int end = request.IndexOf(endStr) - (endStr.Length + 1);
+
+                request = request.Substring(start, end).Trim();
+                client.Close();
+
+                if (count == 10)
+                {
+                    ThreadPool.QueueUserWorkItem(delegate
+                    {
+                        onReceiveCommand(request);
+                    }, request);
+                    count = 0;
+                }
+            });
+
+            while (true)
+            {
+                client = Listener.AcceptSocket();
+                childSocketThread.Start();
+            }
+        }
+    }
+}
