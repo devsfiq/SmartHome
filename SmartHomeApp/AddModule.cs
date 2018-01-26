09196867aca50dcc5edde276f8285c4e27378d9d
Junhong Wee <weejunhong@gmail.com>
2018-01-26 13:44:24 +0800

    Combined Main Form and Add Modules
    Yay

---
+++ SmartHomeApp/AddModule.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmartHomeApp
+{
+    public partial class AddModule : Form
+    {
+        public AddModule()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            openFileDialogModule.Filter = "exe files (*.exe)|*.exe|All files (*.*)|*.*";
+            openFileDialogModule.FilterIndex = 2;
+            openFileDialogModule.RestoreDirectory = true;
+
+            if (openFileDialogModule.ShowDialog() == DialogResult.OK)
+            {
+                string strfilename = openFileDialogModule.InitialDirectory + openFileDialogModule.FileName;
+                tbOpenDialog.Text = strfilename;
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            TextWriter txt = new StreamWriter(System.IO.Path.GetDirectoryName(Application.ExecutablePath) + "\\modules.txt", append: true);
+            try
+            {
+                txt.WriteLine(tbCommandName.Text + "|" + openFileDialogModule.FileName + "|" + tbArgs.Text);
+                txt.Close();
+                MessageBox.Show("Added module successfully!", "Success");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unable to add module! Exception: " + ex.ToString(), "Error");
+            }
+
+        }
+    }
+}
--- SmartHomeApp/MainForm.Designer.cs
+++ SmartHomeApp/MainForm.Designer.cs
@@ -30,14 +30,17 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.OutputTB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAddModule = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Location = new System.Drawing.Point(14, 91);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(118, 17);
+            this.label1.Size = new System.Drawing.Size(89, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Command Output";
             // 
@@ -47,22 +50,45 @@
             this.OutputTB.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.OutputTB.Location = new System.Drawing.Point(16, 33);
+            this.OutputTB.Location = new System.Drawing.Point(11, 109);
+            this.OutputTB.Margin = new System.Windows.Forms.Padding(2);
             this.OutputTB.Multiline = true;
             this.OutputTB.Name = "OutputTB";
             this.OutputTB.ReadOnly = true;
             this.OutputTB.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.OutputTB.Size = new System.Drawing.Size(594, 268);
+            this.OutputTB.Size = new System.Drawing.Size(443, 134);
             this.OutputTB.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Add Module:";
+            // 
+            // btnAddModule
+            // 
+            this.btnAddModule.Location = new System.Drawing.Point(87, 21);
+            this.btnAddModule.Name = "btnAddModule";
+            this.btnAddModule.Size = new System.Drawing.Size(75, 23);
+            this.btnAddModule.TabIndex = 4;
+            this.btnAddModule.Text = "Add";
+            this.btnAddModule.UseVisualStyleBackColor = true;
+            this.btnAddModule.Click += new System.EventHandler(this.btnAddModule_Click);
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(622, 313);
+            this.ClientSize = new System.Drawing.Size(466, 254);
+            this.Controls.Add(this.btnAddModule);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.OutputTB);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "MainForm";
             this.Text = "Smart Home";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
@@ -75,6 +101,8 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox OutputTB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAddModule;
     }
 }
 
